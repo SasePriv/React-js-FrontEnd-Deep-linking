23f9b1c886892831f90bba374146564ddc81280b
Luis Sanchez <sase.priv@gmail.com>
2021-01-28 21:26:31 -0400

    adding https

--- src/pages/personalPage/personalPage.js
+++ src/pages/personalPage/personalPage.js
@@ -10,7 +10,7 @@ const PersonalPage = () => {
     const { id } = useParams();
 
     useEffect(async () => {
-        const getData = await Axios.post('http://amagpieinthesky.com:5000/getName', {name: id})
+        const getData = await Axios.post('https://amagpieinthesky.com:5000/getName', {name: id})
 
         console.log(getData.data)
 
@@ -23,10 +23,10 @@ const PersonalPage = () => {
     },[])
 
     const handleShareBtn = async() => {
-        const getData = await Axios.post('http://amagpieinthesky.com:5000/changestatus', {name: id})
+        const getData = await Axios.post('https://amagpieinthesky.com:5000/changestatus', {name: id})
         console.log(getData.data)
 
-        Axios.post('http://amagpieinthesky.com:5000/generatedynamiclink', {name: id})
+        Axios.post('https://amagpieinthesky.com:5000/generatedynamiclink', {name: id})
         .then((resp) => {
 
             if(resp.data.message !== 'error with the server'){
